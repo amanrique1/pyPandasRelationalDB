c6d6ae3107214b346034cb7cca67dc1a72ad5ff9
Andrés Manrique <andress0199@outlook.com>
2023-03-11 22:44:47 -0600

    Logging fixed

--- pyPandasRelationalDB/__main__.py
+++ pyPandasRelationalDB/__main__.py
@@ -1,4 +1,9 @@
+import logging
 from pyPandasRelationalDB.test import testCase1
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 if __name__ == '__main__':
-    print('Hello World')
+    logging.debug('Starting package execution')
     testCase1()
+    logging.debug('Ending package execution')
